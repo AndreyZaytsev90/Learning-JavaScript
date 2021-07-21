931fc42e8d41875892abe30025a4f84e731e5748
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-07-21 21:16:05 +0300

    added new files

---
+++ Examples/1/TestForJokes.js
@@ -0,0 +1,9 @@
+// Movie Night
+var zipCode = 98104;
+var joesFavoriteMovie = "Forbidden Planet"
+var movieTicket$ = 9;
+if (movieTicket$ >= 9) {
+ alert("Too much!");
+} else {
+ alert("We're going to see " + joesFavoriteMovie)
+}
---
+++ Examples/1/TestForJokes2.js
@@ -0,0 +1,11 @@
+//Test for jokes
+var joke = JavaScriptWalkedIntoABar
+var toldJoke = false
+var $punchline =
+ "Better watch out for those semi-colons."
+var entage = 20;
+var result
+if (toldJoke == true) {
+ Alert($punchline);
+} else
+ alert(joke);
