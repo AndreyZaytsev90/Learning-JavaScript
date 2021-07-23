6909682da2e62386a6f010a24568825d4c612fc6
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-07-23 18:38:03 +0300

    added new files

---
+++ Examples/1/test2.js
@@ -0,0 +1,31 @@
+// const scoops = 3
+// if (scoops < 3) {
+//     alert("Ice cream is running low!");
+//     }
+// if (scoops >= 5) {
+//     alert("Eat faster, the ice cream is going to melt!");
+//     } else if (scoops < 3) {
+//     alert("Ice cream is running low!");
+//     }
+// if (scoops >=5) {
+//     alert ("Eat faster, the ice cream is going to melt!")
+// }
+// else if (scoops ==3) {
+//     alert("Ice cream is running low!")
+// }
+//  else if (scoops == 2) {
+//     alert("Going once!");
+//     } else if (scoops == 1) {
+//     alert("Going twice!");
+//     } else if (scoops == 0) {
+//     alert("Gone!");
+//     } else {
+//     alert("Still lots of ice cream left, come and get it.")
+//     }
+
+const name = "Joe"
+const i = 0
+while (i < 2) {
+document.write("Happy Birthday to you.<br>")
+}
+i = i + 1
