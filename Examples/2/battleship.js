efe638767e10c481d785e4f9f7d65b3f66530088
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-08-02 21:24:25 +0300

    change file battleship.js

--- Examples/2/battleship.js
+++ Examples/2/battleship.js
@@ -1,21 +1,36 @@
-const location1 = 3
-const location2 = 4
-const location3 = 5
+const location1 = 3;
+const location2 = 4;
+const location3 = 5;
 
-let guess
-let hits = 0
-let guesses = 0
+let guess;
+let hits = 0;
+let guesses = 0;
 
-let isSunk = false
+let isSunk = false;
 
-// if (guess == location1) {
-//     hits = hits + 1
-//     } else if (guess == location2) {
-//     hits = hits + 1
-//     } else if (guess == location3) {
-//     hits = hits + 1
-//     }
+while (isSunk == false) {
+  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number!");
+  } else {
+    guesses = guesses + 1;
 
-if (guess == location1 || guess == location2 || guess == location3 ) {
-    hits = hits + 1
+    if (guess == location1 || guess == location2 || guess == location3) {
+      hits = hits + 1;
+      alert("HIT!");
+    } else {
+      alert("MISS!");
+    }
+    if (hits == 3) {
+      isSunk = true;
+      alert("You sank my battleship!");
+    }
+  }
 }
+let stars =
+  "You took " +
+  guesses +
+  " guesses to sink the battleship, " +
+  "which means your shooting accuracy was " +
+  3 / guesses;
+alert(stars);
