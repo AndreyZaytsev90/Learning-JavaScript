334a7ed74c1908ff4eeb643386efe1fc4b3fb369
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-08-02 19:34:40 +0300

    change file battleship.js

--- Examples/2/battleship.js
+++ Examples/2/battleship.js
@@ -7,3 +7,15 @@ let hits = 0
 let guesses = 0
 
 let isSunk = false
+
+// if (guess == location1) {
+//     hits = hits + 1
+//     } else if (guess == location2) {
+//     hits = hits + 1
+//     } else if (guess == location3) {
+//     hits = hits + 1
+//     }
+
+if (guess == location1 || guess == location2 || guess == location3 ) {
+    hits = hits + 1
+}
