ca90715524717c037727e4678f3b143e83c165b3
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-08-19 22:22:31 +0300

    change temperature.js

--- Examples/3/temperature.js
+++ Examples/3/temperature.js
@@ -1,13 +1,18 @@
-function whatShallIWear(temp) {
-  if (temp < 60) {
-    console.log("Wear a sweater");
-  } else if (temp < 70) {
-    console.log("Wear a jacket");
-  } else {
-    console.log("Wear t-shirt");
-  }
-}
+// function whatShallIWear(temp) {
+//   if (temp < 60) {
+//     console.log("Wear a sweater");
+//   } else if (temp < 70) {
+//     console.log("Wear a jacket");
+//   } else {
+//     console.log("Wear t-shirt");
+//   }
+// }
 
-whatShallIWear(60);
-whatShallIWear(80);
-whatShallIWear(50);
+// whatShallIWear(60);
+// whatShallIWear(80);
+// whatShallIWear(50);
+
+function makeTea(cups, tea) {
+  console.log("Brewing " + cups + " cups of " + tea);
+}
+makeTea(3);
