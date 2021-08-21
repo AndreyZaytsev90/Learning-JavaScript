3191184d7c38c046fbb0e69ab72369f68f91e4b3
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-08-21 13:57:05 +0300

    change file temperature.js

--- Examples/3/temperature.js
+++ Examples/3/temperature.js
@@ -12,7 +12,33 @@
 // whatShallIWear(80);
 // whatShallIWear(50);
 
-function makeTea(cups, tea) {
-  console.log("Brewing " + cups + " cups of " + tea);
+// function makeTea(cups, tea) {
+//   console.log("Brewing " + cups + " cups of " + tea);
+// }
+// makeTea(3);
+
+// function makeTea(cups, tea) {
+//   console.log("Brewing " + cups + " cups of " + tea);
+// }
+// makeTea(3, "Earl Grey", "hey ma!", 42);
+
+// function barkAtTheMoon() {
+//   console.log("Woooooooooooooo!");
+// }
+// barkAtTheMoon();
+
+function bake(degrees) {
+  let message;
+  if (degrees > 500) {
+    message = "I'm not a nuclear reactor!";
+  } else if (degrees < 100) {
+    message = "I'm not a refrigerator!";
+  } else {
+    message = "That's a very comfortable temperature for me.";
+    //setMode("bake");
+    //setTemp(degrees);
+  }
+  return message;
 }
-makeTea(3);
+let status = bake(350);
+console.log(status);
