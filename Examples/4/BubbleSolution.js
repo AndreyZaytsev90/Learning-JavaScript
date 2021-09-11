b36b877942713a86d57adf50dd64ce232041d55a
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-09-11 16:04:01 +0300

    change file BubbleSolution.js

--- Examples/4/BubbleSolution.js
+++ Examples/4/BubbleSolution.js
@@ -1,10 +1,17 @@
-var i = 0;
-var hasBubbleGum = [false, false, false, true];
-var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+//var i = 0;
+let hasBubbleGum = [false, false, false, true];
+let products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
 
-while (i < hasBubbleGum.length) {
+//while (i < hasBubbleGum.length) {
+//  if (hasBubbleGum[i]) {
+//    console.log(products[i] + " contains bubble gum");
+//  }
+//  i = i + 1;
+//}
+// с циклом for
+
+for (let i = 0; i < hasBubbleGum.length; i = i + 1) {
   if (hasBubbleGum[i]) {
     console.log(products[i] + " contains bubble gum");
   }
-  i = i + 1;
 }
