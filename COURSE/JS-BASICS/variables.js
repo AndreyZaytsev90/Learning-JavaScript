66dc0f31c8ce6efbc70aa99f14ed538a7e592021
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-10 20:51:33 +0300

    changed file

---
+++ COURSE/JS-BASICS/variables.js
@@ -0,0 +1,18 @@
+var name = "Andrey";
+console.log(name);
+name = "OIlga";
+console.log(name);
+
+var color;
+console.log(color);
+
+color = "red";
+console.log(color);
+
+//Javascript names can include
+//letters, numbers, $, _
+
+myMainColor = "green"; // camel case
+my_main_color; // snake case
+my - main - color; // kebab case
+MyMainColor; // Pascal case
