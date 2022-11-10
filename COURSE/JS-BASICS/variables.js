6177437d6a6f125d1c09aac8f58551c86f42a0dc
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-10 21:13:13 +0300

    changed file

--- COURSE/JS-BASICS/variables.js
+++ COURSE/JS-BASICS/variables.js
@@ -1,18 +1,44 @@
-var name = "Andrey";
-console.log(name);
-name = "OIlga";
-console.log(name);
+// var name = "Andrey";
+// console.log(name);
+// name = "OIlga";
+// console.log(name);
 
-var color;
-console.log(color);
+// var color;
+// console.log(color);
 
-color = "red";
-console.log(color);
+// color = "red";
+// console.log(color);
 
 //Javascript names can include
 //letters, numbers, $, _
 
-myMainColor = "green"; // camel case
-my_main_color; // snake case
-my - main - color; // kebab case
-MyMainColor; // Pascal case
+// myMainColor = "green"; // camel case
+// my_main_color; // snake case
+// my - main - color; // kebab case
+// MyMainColor; // Pascal case
+
+const name = "Andrey";
+console.log(name);
+// name = "Olga"; //-> error
+
+const color = "red";
+console.log(color);
+
+const user = {
+  name: "Andrey",
+  gender: "male",
+  age: 32,
+};
+
+// console.log(user);
+// user.age = 33;
+// console.log(user.age);
+// user.weight = 93;
+// console.log(user);
+
+const colors = ["white", "red", "black"];
+console.log(colors);
+
+colors[3] = "blue";
+
+console.log(colors);
