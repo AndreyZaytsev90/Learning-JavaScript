d0220219bb0f540676f8f3004fb6d6d2d1444659
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-15 22:10:48 +0300

    added new file

---
+++ COURSE/COMPARISONOPERATORS/script.js
@@ -0,0 +1,31 @@
+const x = 3;
+const y = 5;
+let isXMoreThanY = x > y;
+
+console.log(isXMoreThanY);
+
+const a = 3;
+const b = 5;
+let isXMoreThanY2 = x <= y;
+
+console.log(isXMoreThanY2);
+
+//!= неравенство
+
+const a1 = 3;
+const b2 = 5;
+let isXMoreThanY3 = x != y;
+
+console.log(isXMoreThanY3);
+
+const a2 = "3";
+const b3 = 5;
+let isXMoreThanY4 = a2 !== b3;
+
+console.log(isXMoreThanY4);
+
+console.log(true === 1); //false разные типы
+console.log(true == 1); // true
+
+console.log(null == undefined); // true
+console.log(NaN == NaN); // false
