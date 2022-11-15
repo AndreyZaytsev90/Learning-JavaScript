74385614474763fb496fb87b2314550009ce13b4
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-15 21:46:03 +0300

    changed file

--- COURSE/QUESTIONRYTASK/script.js
+++ COURSE/QUESTIONRYTASK/script.js
@@ -1,8 +1,21 @@
-let favoriteColor;
+// let favoriteColor;
 
-//alert("My favorite color is " + favoriteColor);
+// //alert("My favorite color is " + favoriteColor);
 
-favoriteColor = prompt("What is your favorite color?");
+// favoriteColor = prompt("What is your favorite color?");
 
-console.log("My favorite color is " + favoriteColor);
-alert("My favorite color is " + favoriteColor);
+// console.log("My favorite color is " + favoriteColor);
+// alert("My favorite color is " + favoriteColor);
+
+let firstName;
+let lastName;
+let age;
+
+firstName = prompt("What is your firstname?");
+console.log("My firstname is " + firstName);
+
+lastName = prompt("What is your lastname?");
+console.log("My firstname is " + lastName);
+
+age = prompt("How old are you?");
+console.log("My firstname is " + age);
