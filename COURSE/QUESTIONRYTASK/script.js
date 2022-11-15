850fd82b3f0d6341a8ec2bb5b89f2a6f93c3d9a3
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-15 21:31:57 +0300

    added new file

---
+++ COURSE/QUESTIONRYTASK/script.js
@@ -0,0 +1,8 @@
+let favoriteColor;
+
+//alert("My favorite color is " + favoriteColor);
+
+favoriteColor = prompt("What is your favorite color?");
+
+console.log("My favorite color is " + favoriteColor);
+alert("My favorite color is " + favoriteColor);
