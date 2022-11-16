06d8b5513d3ed82593d5814128ff5ddf8b647e12
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-16 22:11:16 +0300

    changed file

--- COURSE/19-PRECEDENCEOPERATOR/script.js
+++ COURSE/19-PRECEDENCEOPERATOR/script.js
@@ -5,9 +5,28 @@
 
 // console.log(y);
 
-let x, y;
+// let x, y;
 
-console.log(x, y);
+// console.log(x, y);
 
-x = y = 10 + 5 - 3 * 4;
-console.log(x, y);
+// x = y = 10 + 5 - 3 * 4;
+// console.log(x, y);
+
+/////////////////Сокращенная запись операторов
+
+let x = 5;
+x = x + 5;
+// ===
+x += 5;
+x -= 2;
+x *= 3;
+x /= 3;
+
+x += 1;
+//===
+x++;
+x--;
+
+++x;
+--x;
+console.log(x);
