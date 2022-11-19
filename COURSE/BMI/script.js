392c58de01b58df26b59c9f94d9166a1a31f057c
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-19 17:15:53 +0300

    changed file

--- COURSE/BMI/script.js
+++ COURSE/BMI/script.js
@@ -1,13 +1,32 @@
-let weight;
-let height;
+// let weight;
+// let height;
 
-console.log(alert("Здравствуйте! Вас преветствует калькулятор массы тела"));
-weight = prompt("Указите свой вес в килограммах:");
-height = prompt("Указите свой рост в метрах:");
-let bmi = weight / (height * height);
-const result =
-  bmi <= 24.9
-    ? "У вас нормальный вес"
-    : "У вас большой вес! Обратитесь к врачу!";
+// console.log(alert("Здравствуйте! Вас приветствует калькулятор массы тела"));
+// weight = prompt("Указите свой вес в килограммах:");
+// height = prompt("Указите свой рост в метрах:");
+// let bmi = weight / (height * height);
+// const result =
+//   bmi <= 24.9
+//     ? "У вас нормальный вес"
+//     : "У вас большой вес! Обратитесь к врачу!";
 
-alert(result);
+// alert(result);
+
+//With If-Else statment
+
+alert("Здравствуйте! Вас приветствует калькулятор массы тела");
+const weight = prompt("Указите свой вес в килограммах:");
+const height = prompt("Указите свой рост в метрах:");
+let bmi = weight / Math.pow(height, 2);
+// const result =
+//   bmi <= 24.9
+//     ? "У вас нормальный вес"
+//     : "У вас большой вес! Обратитесь к врачу!";
+
+if (bmi <= 24.9) {
+  alert("У вас нормальный вес!");
+  console.log("У вас нормальный вес!");
+} else {
+  alert("У вас большой вес! Обратитесь к врачу!");
+  console.log("У вас большой вес! Обратитесь к врачу!");
+}
