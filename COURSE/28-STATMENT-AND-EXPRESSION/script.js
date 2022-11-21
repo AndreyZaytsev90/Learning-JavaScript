5fdb9ba655b350f77e8c243cf20f15306cc19028
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-21 22:15:03 +0300

    added new file

---
+++ COURSE/28-STATMENT-AND-EXPRESSION/script.js
@@ -0,0 +1,13 @@
+// Expressions - производят значения
+
+2 + 2;
+37;
+true && false;
+
+//Statement - как предложения
+
+const number = 10;
+
+if (number > 10) {
+  const text = "The number is positive";
+}
