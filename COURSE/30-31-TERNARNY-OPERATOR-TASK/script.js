4c35d30f9f180b11ddc2c6e109cca1ff94f52f85
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-11-22 21:46:26 +0300

    added new file

---
+++ COURSE/30-31-TERNARNY-OPERATOR-TASK/script.js
@@ -0,0 +1,19 @@
+
+//1
+// const userAge = prompt('Enter your age:') >= 21 
+// ? alert('You can buy the alcohol') 
+// : alert('You can\'t buy the alcohol, you are too young') 
+
+// userAge >= 21 
+// ? console.log('You can buy the alcohol')
+// : console.log('You can\'t buy the alcohol, you are too young');
+
+
+//2
+const userAge = prompt('Enter your age:')
+const message = userAge >= 21 
+? 'You can buy the alcohol.' 
+: 'You can\'t buy the alcohol, you are too young.'
+
+console.log(message);
+alert(message)
---
+++ COURSE/33-STRICT-MODE/script.js
@@ -0,0 +1,14 @@
+//Srtict mode - безопасный JS код, предупреждает об ошибках
+
+'use strict' // писать вначале кода
+
+let hasCertificate = false
+const passExam = false
+
+passExam ? hasCertificate = true : ''
+
+console.log(hasCertificated);
+
+//const interface = 'video' //Uncaught SyntaxError: Unexpected strict mode reserved word
+
+const package = 'MyPackage' // Uncaught SyntaxError: Unexpected strict mode reserved word
