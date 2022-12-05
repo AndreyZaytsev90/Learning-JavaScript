725c52044bc243835da5ee1ec89d699a369bdfc0
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-12-05 22:06:27 +0300

    added new file

---
+++ COURSE/45-OBJECTS/script.js
@@ -0,0 +1,12 @@
+personData = ["Andrey", "Zaytsev", 32, "Lead engineer", ["Olga", "Petr"]];
+
+myData = {
+  firstName: "Andrey",
+  lastName: "Zaytsev",
+  age: 32,
+  job: "Lead engineer",
+  myFamily: ["Olga", "Petr"],
+};
+
+console.log(myData.age);
+console.log(myData.myFamily[1]);
