12a284614a082d570350eb41513ae072abcefbda
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-12-16 23:18:17 +0300

    update file

--- COURSE/50-56-LOOPS/script.js
+++ COURSE/50-56-LOOPS/script.js
@@ -1,12 +1,20 @@
-//50
+//50 For
 /*console.log('Push 1')
 console.log('Push 2')
 console.log('Push 3')
 console.log('Push 4')
 console.log('Push 5')*/
 
-for (let push = 5; push <= 10; push+=2) {
+/*for (let push = 5; push <= 10; push+=2) {
     console.log(`Push ${push}`)
-}
+}*/
 
-//51
+//51 Loops and Arrays
+
+//Итерация (перебирание) элементов массива
+
+const user = personData = ["Andrey", "Zaytsev", 1990, "Lead engineer", ["Olga", "Petr"]];
+
+for (let i = 0; i <= user.length-1; i++) {
+    console.log(user[i], typeof user[i])
+}
