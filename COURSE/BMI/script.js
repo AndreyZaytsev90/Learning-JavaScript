edbc74858de38acd9900477c710a641c1e485464
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-12-16 22:58:50 +0300

    update file

--- COURSE/BMI/script.js
+++ COURSE/BMI/script.js
@@ -15,8 +15,8 @@
 //With If-Else statment
 
 alert("Здравствуйте! Вас приветствует калькулятор массы тела");
-const weight = prompt("Указите свой вес в килограммах:");
-const height = prompt("Указите свой рост в метрах:");
+const weight = prompt("Укажите свой вес в килограммах:");
+const height = prompt("Укажите свой рост в метрах:");
 let bmi = weight / Math.pow(height, 2);
 // const result =
 //   bmi <= 24.9
@@ -28,12 +28,12 @@ if (bmi <= 24.9) {
   console.log("У вас нормальный вес!");
 } else {
   alert(
-    `У вас большой вес! Индекс BMI привышен на ${Math.round(
+    `У вас большой вес! Индекс BMI превышен на ${Math.round(
       bmi - 24.9
     )} Обратитесь к врачу!`
   );
   console.log(
-    `У вас большой вес! Индекс BMI привышен на ${Math.round(
+    `У вас большой вес! Индекс BMI превышен на ${Math.round(
       bmi - 24.9
     )} Обратитесь к врачу!`
   );
