56d52ce9aa05227af4e86b196e36d0e884489b4a
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-12-16 22:58:21 +0300

    added new file

---
+++ COURSE/50-56-LOOPS/script.js
@@ -0,0 +1,12 @@
+//50
+/*console.log('Push 1')
+console.log('Push 2')
+console.log('Push 3')
+console.log('Push 4')
+console.log('Push 5')*/
+
+for (let push = 5; push <= 10; push+=2) {
+    console.log(`Push ${push}`)
+}
+
+//51
