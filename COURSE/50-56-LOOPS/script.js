c3cfc4b5f6680b4fc37fa8165873621699c1be33
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2022-12-17 20:41:52 +0300

    update file

--- COURSE/50-56-LOOPS/script.js
+++ COURSE/50-56-LOOPS/script.js
@@ -15,6 +15,24 @@ console.log('Push 5')*/
 
 const user = personData = ["Andrey", "Zaytsev", 1990, "Lead engineer", ["Olga", "Petr"]];
 
+const types = []
+
+
 for (let i = 0; i <= user.length-1; i++) {
     console.log(user[i], typeof user[i])
+   // types[i] = typeof user[i]
+    types.push(typeof user[i]) // заполнение массива типами элементов другого массива
 }
+
+console.log(types)
+
+// -------------------------------------------------
+
+const birthYears = [1990, 1988, 2018]
+const ages = []
+
+for (let i = 0; i < birthYears.length; i++) {
+    ages.push(2022 - birthYears[i])
+}
+
+console.log(ages)
