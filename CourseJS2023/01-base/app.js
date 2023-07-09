0a01d4a50708dc4992530c65800238e8f8872ead
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-07-09 12:33:29 +0300

    Update file

---
+++ CourseJS2023/01-base/app.js
@@ -0,0 +1,28 @@
+let num = 42;
+let firstName = "Andrey";
+const isProgrammer = true;
+
+//can do
+/*
+let $ = "text";
+let $num = 43;
+let num$ = 44;
+let _ = 50;
+let _num = 45;
+let num_ = 46;
+const first_name = "Andrey"; //bad
+const myNum = 7; // good
+let num7 = '7'
+*/
+
+//restricted
+/*
+//let 7num = '7'
+//let my-num = 7
+//let const
+*/
+
+//firstName = "Olga";
+//isProgrammer = false; // error because is const
+
+alert(num);
