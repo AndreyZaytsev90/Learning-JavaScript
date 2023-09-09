5c85d4b7ae44b84ffa285771a9af924586858eb2
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-09-09 21:01:09 +0300

    Update file

--- CourseJS2023/-classes/app.js
+++ CourseJS2023/-classes/app.js
@@ -1,14 +1,26 @@
 class Box {
   // создали класс
+  _age = 0;
+
   constructor(name, age) {
     // вызывается сразу после создания класса
     console.log("Создаем класс");
     this.name = name; // переносим name и age в глобальную область видимости
-    this.age = age;
+    this.age = age; // значение переменной age попадает в value сеттера
   }
   hi() {
     // функция - это МЕТОД внутри класса
     console.log("Hello " + this.name); // this адресован к экземпляру класса Box
+  }
+
+  set age(value) {
+    //сеттер
+    this._age = value;
+    console.log(2023 - this._age);
+  }
+  get age() {
+    // геттер
+    return this._age;
   }
 }
 // Инициализируем класс (создаем его копию)
@@ -26,3 +38,18 @@ console.log(myBox2.name);
 myBox.hi();
 myBox2.hi();
 myBox3.hi();
+
+class Example {
+  constructor() {
+    this.message = "Hello, World!";
+  }
+
+  get helloMessage() {
+    return this.message;
+  }
+}
+
+const example = new Example();
+console.log(example.helloMessage);
+
+console.log(myBox instanceof Box); // проверка, что экземпляр класс создан именно от класс Box
